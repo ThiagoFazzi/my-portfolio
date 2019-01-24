d28968771c12ffd625946ed142c04306138df4e2
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-24 13:45:03 +0100

    Updates

---
+++ precache-manifest.60bb03b61dad1f2db6cfff2137b38e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49b01cbe532ed087675150e31f823746",
+    "url": "/my-portfolio/static/media/thiago-photo.49b01cbe.jpg"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "3fbd2e5a934f08f2ac84",
+    "url": "/my-portfolio/static/js/main.3fbd2e5a.chunk.js"
+  },
+  {
+    "revision": "1df02932dcaff48f25c3",
+    "url": "/my-portfolio/static/js/1.1df02932.chunk.js"
+  },
+  {
+    "revision": "3fbd2e5a934f08f2ac84",
+    "url": "/my-portfolio/static/css/main.97502c7f.chunk.css"
+  },
+  {
+    "revision": "cc8eff35a3ded2a9dcd0ca34a9884549",
+    "url": "/my-portfolio/index.html"
+  }
+];
